9c4d45b393e9bd23af6312421789e2f49d4bc354
Jack Howard <jackhowaa@gmail.com>
2019-07-20 09:12:57 -0500

    Initial Commit

---
+++ landmark-demo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  landmark-demo
+//
+//  Created by Jack Howard on 7/20/19.
+//  Copyright © 2019 Jack Howard. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
